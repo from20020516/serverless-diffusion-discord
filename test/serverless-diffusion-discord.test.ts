b1746b37a546fd000e25c0dc1dbd0972d76ea5d6
from20020516 <from20020516@gmail.com>
2022-10-27 19:36:51 +0900

    Initial commit

---
+++ test/serverless-diffusion-discord.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessDiffusionDiscord from '../lib/serverless-diffusion-discord-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-diffusion-discord-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessDiffusionDiscord.ServerlessDiffusionDiscordStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
